8f4f28f9b95ebacae5b55646ddb0f6ec071ed8d8
isabelffernandes <isabelffernandes@yahoo.com.br>
2021-12-01 15:07:21 -0300

    tst

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from subprocess import check_output
+
 import sys
 
 def get_merged_branches():
 
-    raw_results = check_output('git branch --merged master', shell=True)
+    raw_results = check_output('git branch --merged master', 
+    shell=True)
     
     branchs = []
     for branch in raw_results.decode('utf-8').strip().split('\n'):
         if not branch.startswith('*') and branch.strip() not in ['master', 'staging', 'sprint', 'dev', 'main']:
             branchs.append(branch.strip()) 
+
 
     return branchs
 
